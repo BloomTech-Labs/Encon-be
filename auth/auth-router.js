90471b7d10c4e2a1ae8f546e7f66cc7fb7fce772
Ben <bbjberg91@gmail.com>
2020-06-18 00:59:25 -0400

    deploy

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,15 +21,6 @@ router.post('/register', (req,res) => {
 
 });
 
-router.get('/', (req, res) => {
-  Users.find()
-  .then(users => {
-    res.json(users);
-  })
-  .catch(err => {
-    res.status(500).json({ message: err.message });
-  });
-});
 
 router.post('/login', (req, res) => {
   let { email , password } = req.body;
